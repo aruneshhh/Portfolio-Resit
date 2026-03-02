c3dcfa5808b5ba823bb2939f3eafbc372d58f774
Arunesh <aruneshv2002@gmail.com>
2026-03-02 11:16:53 +0100

    final changes

--- item7.R
+++ item7.R
@@ -1,5 +1,4 @@
 # MODEL 1: Generalized Linear Model (Logistic Regression)
-# We interpret this as: "How does the PROBABILITY of diabetes change?"
 diabetes_data <- read.csv("DATASET/diabetes.csv")
 cols_with_zeros <- c("Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI")
 diabetes_data[cols_with_zeros] <- lapply(diabetes_data[cols_with_zeros], function(x) ifelse(x == 0, NA, x))
@@ -10,7 +9,6 @@ glm_model <- glm(Outcome ~ Glucose + BMI + Age,
                  family = "binomial")
 
 # MODEL 2: Ordinary Least Squares (Linear Regression)
-# We interpret this as: "Fitting a straight line through the data points 
 lm_model <- lm(Outcome ~ Glucose + BMI + Age, 
                data = diabetes_data)
 
--- item9.R
+++ item9.R
@@ -1,11 +1,6 @@
 if(!require(dagitty)) install.packages("dagitty")
 library(dagitty)
 
-# We define a complex structure involving almost all variables:
-# 1. "Confounders" (Age, Pregnancies, Pedigree) cause BMI and Outcome.
-# 2. "Mediators" (Glucose, BP, Insulin) are caused by BMI and cause Outcome.
-# 3. We want to know: Is there still a direct arrow from BMI -> Outcome?
-
 dag_model <- dagitty("dag {
     Age -> BMI
     Age -> Outcome
@@ -22,31 +17,20 @@ dag_model <- dagitty("dag {
     BMI -> Outcome [outcome, exposure]
 }")
 
-# ---------------------------------------------------------
-# STEP 2: Derive the Adjustment Set (For DIRECT Effect)
-# ---------------------------------------------------------
-# We ask: "What must I control to see the DIRECT effect of BMI, 
-# blocking the paths through Glucose, BP, etc.?
+plot(dag_model)
+
 
 adj_set <- adjustmentSets(dag_model, exposure = "BMI", outcome = "Outcome", effect = "direct")
 
-print("--- Required Adjustment Set for Direct Effect ---")
+print(" Adjustment Set for Direct Effect")
 print(adj_set) 
-# The output will tell us to adjust for EVERYTHING: 
-# {Age, Pregnancies, Pedigree, Glucose, BP, Insulin, SkinThickness}
 
-# ---------------------------------------------------------
-# STEP 3: Perform Causal Analysis (Direct Effect)
-# ---------------------------------------------------------
 
-# We fit the full model with all variables
 causal_model <- glm(Outcome ~ BMI + Age + Pregnancies + DiabetesPedigreeFunction + 
                       Glucose + BloodPressure + Insulin + SkinThickness,
                     data = diabetes_data, 
                     family = "binomial")
 
-# ---------------------------------------------------------
-# STEP 4: Interpret the Result
-# ---------------------------------------------------------
+
 print("--- Direct Causal Effect of BMI (Coefficients) ---")
 print(summary(causal_model))
